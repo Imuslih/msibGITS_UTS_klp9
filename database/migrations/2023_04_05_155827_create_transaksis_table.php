c32ef6d4ffbd0a54266202e62d28a7ebcbc53edb
radisyah <syahputradimas965@gmail.com>
2023-04-06 17:08:33 +0700

    first

---
+++ database/migrations/2023_04_05_155827_create_transaksis_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaksis', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('detailTransaksi_id');
+            $table->foreign('detailTransaksi_id')->references('id')->on('detail_transaksis');
+            $table->string('invoice');
+            $table->unsignedInteger('total_price')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaksis');
+    }
+};
