ee2a94914e557e1a3d1dcd78eb7361e98ab411f8
radisyah <syahputradimas965@gmail.com>
2023-04-07 20:38:01 +0700

    tambah_tampilan

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -4,5 +4,39 @@
 
 @section('contents')
 
-
+<div class="col-md-12">
+  <div class="card">
+    <div class="card-header">
+      <h3 class="card-title">Kategori</h3>
+        <div class="card-tools">
+          <button type="button" data-target="" class="btn btn-primary btn-sm btn-flat"><i class="fas fa-plus"></i>Add</button>
+        </div>
+    </div>
+    <!-- /.card-header -->
+    <div class="card-body">   
+      <table id="example1" class="table table-bordered table-striped text-center">
+        <thead>
+          <tr >
+            <th width="50px">No</th>
+            <th>Kategori</th>
+            <th width="100px">Aksi</th>
+          </tr>
+        </thead>
+       
+        <tbody>
+          <tr class="">
+            <td>Coba</td>
+            <td>Coba</td>
+            <td>
+            <button data-toggle="modal" data-target="" class="btn btn-sm btn-warning">Edit</button>
+            <button data-toggle="modal" data-target="" class="btn btn-sm btn-danger">Delete</button>
+            </td>
+          </tr>
+        </tbody>
+       
+      </table>
+    </div>
+    <!-- /.card-body -->
+  </div>
+</div>
 @endsection
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -3,6 +3,51 @@
 @section('title', 'Data Barang')
 
 @section('contents')
-
-
+<div class="col-md-12">
+  <div class="card">
+    <div class="card-header">
+      <h3 class="card-title">Produk</h3>
+        <div class="card-tools">
+          <button type="button" data-target="" class="btn btn-primary btn-sm btn-flat"><i class="fas fa-plus"></i>Add</button>
+        </div>
+    </div>
+    <!-- /.card-header -->
+    <div class="card-body">   
+      <table id="example1" class="table table-bordered table-striped text-center">
+        <thead>
+          <tr >
+            <th width="50px">No</th>
+            <th>Id</th>
+            <th>Nama Produk</th>
+            <th>Kategori</th>
+            <th>Harga Beli</th>
+            <th>Harga Jual</th>
+            <th>Stok</th>
+            <th>Gambar</th>
+            <th width="100px">Aksi</th>
+          </tr>
+        </thead>
+       
+        <tbody>
+          <tr class="">
+            <td>Coba</td>
+            <td>Coba</td>
+            <td>Coba</td>
+            <td>Coba</td>
+            <td>Coba</td>
+            <td>Coba</td>
+            <td>Coba</td>
+            <td>Coba</td>
+            <td>
+            <button data-toggle="modal" data-target="" class="btn btn-sm btn-warning">Edit</button>
+            <button data-toggle="modal" data-target="" class="btn btn-sm btn-danger">Delete</button>
+            </td>
+          </tr>
+        </tbody>
+       
+      </table>
+    </div>
+    <!-- /.card-body -->
+  </div>
+</div>
 @endsection
