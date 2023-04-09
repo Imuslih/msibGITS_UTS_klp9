e3faef3db311d22a4e82531494aa9de7327b0c9a
irfansurya <111202012863@mhs.dinus.ac.id>
2023-04-09 22:40:21 +0700

    ketiga

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\TransactionController;
-use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -42,3 +43,11 @@ use App\Http\Controllers\CategoryController;
     Route::get('', 'index')->name('transaction');
   });
 
+  Route::get('login', [AuthController::class, 'index'])->name('login');
+  Route::post('login', [AuthController::class, 'login'])->name('login');
+
+  Route::get('register', [AuthController::class, 'register_view'])->name('register');
+  Route::post('register', [AuthController::class, 'register'])->name('register');
+
+  Route::get('dashboard', [AuthController::class, 'dasboard'])->name('dasboard');
+  Route::get('logout', [AuthController::class, 'logout'])->name('logout');
