22c462f90e9a1163b6225e50afb3b52e17ca6c47
irfansurya <111202012863@mhs.dinus.ac.id>
2023-04-10 21:30:29 +0700

    database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Categories;
+use App\Models\Products;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,5 +18,28 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        User::create([
+            'name'  => 'testing',
+            'email' => 'testing1@gmail.com',
+            'password' => 12345
+        ]);
+
+        Categories::create([
+            'name' => 'Makanan'
+        ]);
+
+        Categories::create([
+            'name' => 'Minuman'
+        ]);
+
+        Products::create([
+            'product_code' => 'K001',
+            'name' => 'Kopi Kapal Api',
+            'purchase_price' => '12000',
+            'selling_price' => '20000',
+            'stock' => '100',
+            'image' => 'public/img/avatar5.png',
+            'category_id' => 1,
+        ]);
     }
 }
