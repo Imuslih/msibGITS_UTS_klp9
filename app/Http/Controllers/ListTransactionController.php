9d704b928b029a3a7710d06eacef8950275989bc
irfansurya <111202012863@mhs.dinus.ac.id>
2023-04-12 00:34:20 +0700

    halaman list transaksi

--- app/Http/Controllers/ListTransactionController.php
+++ app/Http/Controllers/ListTransactionController.php
@@ -7,21 +7,24 @@ use App\Models\User;
 use App\Models\Products;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use App\Models\Transaksi;
 
 class ListTransactionController extends Controller
 {
 
     public function index()
     {
-      $data = array(
-      'menu' => 'list_transaction',
-			'sub_menu' => '',
-			'title' => 'Halaman Riwayat Transaksi',
-			'judul' => 'Riwayat Transaksi',
-			'sub_judul' => '',
-      
-      );
-      return view('list_transaction.index',$data);
+      $data1 = array(
+        'menu' => 'list_transaction',
+        'sub_menu' => '',
+        'title' => 'Halaman Riwayat Transaksi',
+        'judul' => 'Riwayat Transaksi',
+        'sub_judul' => ''
+  
+        );
+      $data_listtransaction=Transaksi::all();
+      //return $data_listtransaction; 
+      return view('list_transaction.index',['data'=>$data_listtransaction],$data1);
     }
 
 
--- resources/views/list_transaction/index.blade.php
+++ resources/views/list_transaction/index.blade.php
@@ -3,6 +3,46 @@
 
 @section('contents')
 
+@extends('layouts.app')
+
+@section('title', 'Data Barang')
+
+@section('contents')
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-heaader">
+                    <div class="card-body">
+                        <table class="table table-striped">
+                            <tr >
+                                <th>Id</th>
+                                <th>User_id</th>
+                                <th>Costumer_name</th>
+                                <th>Costumer_phone</th>
+                                <th>Invoice</th>
+                                <th>Total_price</th>
+                            </tr>
+                                @foreach ($data as $item)
+                                <tr class="">
+                                    <td>{{$item->id}}</td>
+                                    <td>{{$item->user_id}}</td>
+                                    <td>{{$item->costumer_name}}</td>
+                                    <td>{{$item->costumer_phone}}</td>
+                                    <td>{{$item->invoice}}</td>
+                                    <td>Rp. {{$item->total_price}}</td>
+                                </tr>
+                                @endforeach
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
 
   
 
